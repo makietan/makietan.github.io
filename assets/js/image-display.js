a87797a4d8ce26f6df72f112d45a8d92ec57b62c
makietan <makietan@users.noreply.github.com>
2020-02-17 17:47:39 +0900

    onerror で様子見

--- assets/js/image-display.js
+++ assets/js/image-display.js
@@ -1,12 +1,12 @@
 function hide_image() {
   [...document.querySelectorAll('.card__image > img')].forEach((e) => {
-    e.addEventListener('error', () => {
+    e.onerror = () => {
       e.style.display = 'none';
       e.parentElement.style.display = 'none';
       e.parentElement.parentElement.querySelector('.card__title').style.left = '8px';
       e.parentElement.parentElement.querySelector('.card__description').style.left = '8px';
       e.parentElement.parentElement.querySelector('.card__header').style.left = '8px';
-    });
+    };
   });
 }
 document.addEventListener("DOMContentLoaded", (event) => hide_image());
